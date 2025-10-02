6f8d32f38d0a92e64b6a9a35bc6ef4280bdf9ce8
Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
2025-10-02 19:27:38 +0100

    feat(DashBoard.tsx): implementing chart visualisation

--- src/data/mockProgressOverview.ts
+++ src/data/mockProgressOverview.ts
@@ -1,8 +1,8 @@
 export const mockProgressOverview = {
-    weightGoal: 70,          // kg
-    currentWeight: 75,       // kg
-    calorieGoal: 2000,       // kcal/day
-    averageCalories: 2200,   // kcal/day
-    weeklyWorkoutGoal: 5,    // per week
-    completedWorkouts: 3     // per week
+    "weightGoal": 70,          // kg
+    "currentWeight": 75,       // kg
+    "calorieGoal": 2000,       // kcal/day
+    "averageCalories": 2200,   // kcal/day
+    "weeklyWorkoutGoal": 5,    // per week
+    "completedWorkouts": 3     // per week
 };
